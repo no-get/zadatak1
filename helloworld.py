eeabc45e4d8b80411c9c95930c43f0210ecccfb1
David Z <ordergettinglifein@gmail.com>
2025-01-24 15:46:10 +0200

    initial commit for assignment

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+for i in range(0, 36):
+    print("Hello World")
+
+    #Zeleo sam da mogu da vidim izlaz kada ne pokrecem program iz terminala pa sam dodao input
+    if i==35: 
+        input("Pritisnite Enter da bi se program zatvorio...")
